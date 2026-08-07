57c34c3d71ab786687d344d081dd7bc0ae872aaf
ALEKSEYR554 <a.morugov@mail.ru>
2026-08-07 20:25:01 +0400

    discord proxy

--- config/initializers/discord_bot.rb
+++ config/initializers/discord_bot.rb
@@ -5,7 +5,12 @@ if ENV["DISCORD_BOT_TOKEN"].present? && !defined?(Rails::Console)
   Thread.new do
     CHANNEL_ID = 1097158993207631902
     DEBUG_CHANNEL_ID = 1529870425440391198
-    bot = Discordrb::Bot.new(token: ENV["DISCORD_BOT_TOKEN"], intents: :all)
+    # Добавлен параметр proxy
+    bot = Discordrb::Bot.new(
+      token: ENV["DISCORD_BOT_TOKEN"],
+      intents: [ :servers, :server_messages ],
+      proxy: "http://127.0.0.1:56799"
+    )
 
     def sync_servant(message, bot)
       text = message.content
